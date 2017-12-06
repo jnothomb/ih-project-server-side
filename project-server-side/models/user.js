bfe14159d04ed18e708907c7b6b74442e1574cd1
Tiago Ornelas <titasornelas@gmail.com>
2017-12-06 16:48:49 +0100

    typo - oops

--- project-server-side/models/user.js
+++ project-server-side/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -14,7 +15,7 @@ const userSchema = new mongoose.Schema({
     category: String
   }
 
-);
+});
 
 const User = mongoose.model("User", userSchema);
 
