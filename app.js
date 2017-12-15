10a6e94a33d7d7243820ee460a5e21580d13ff58
Jean Nothomb <jeannothomb@gmail.com>
2017-12-15 10:29:20 +0100

    correct again

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
-const path = require("path");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
